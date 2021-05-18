9e737b097345a5f078adc4cc6fcf56693e597fd1
Matt Riese <rise.matt@gmail.com>
2021-05-17 17:11:52 -0700

    working on testing user model

--- models/user.js
+++ models/user.js
@@ -1,96 +1,93 @@
-"use strict";
+'use strict';
 
-const bcrypt = require("bcrypt");
-const { BCRYPT_WORK_FACTOR } = require("./config");
+const bcrypt = require('bcrypt');
+const { BCRYPT_WORK_FACTOR } = require('../config');
+const db = require("../db");
+const { NotFoundError } = require("../expressError");
 
 /** User of the site. */
 
 class User {
-
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
-
     //before starting the insert statement, need to generate the hashed
     //version of the user's password to send in
 
-    const hashedPassword = await bcrypt.hash(
-      password, BCRYPT_WORK_FACTOR);
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
     const result = await db.query(
       `INSERT INTO users (username,
                              password,
                              first_name,
                              last_name,
-                             phone
+                             phone,
+                             join_at
                              )
          VALUES
-           ($1, $2, $3, $4, $5)
+           ($1, $2, $3, $4, $5, current_timestamp)
          RETURNING username, password, first_name, last_name, phone`,
-      [username, hashedPassword, first_name, last_name, phone]);
+      [username, hashedPassword, first_name, last_name, phone]
+    );
 
     return result.rows[0];
-
   }
-
-
-
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
-
-    const hashedPassword = await bcrypt.hash(
-      password, BCRYPT_WORK_FACTOR);
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    console.log("hashed passwrod: ", hashedPassword);
 
     const result = await db.query(
       `SELECT password
                               FROM users
                               WHERE username = $1`,
-      [username]);
+      [username]
+    );
 
     const u = result.rows[0];
+    console.log("u line 50: ", u);
 
-    if (await bcrypt.compare(hashedPassword, u.password) === true) {
+    // if (u === undefined) throw new NotFoundError(`User cannot be found: ${username}`);
+
+    if (await bcrypt.compare(password, u.password) === true) {
       return true;
-    }
-    else {
-      return false;
+    } else {
+      return false; //is this enough or should we do user check
     }
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
-
     const result = await db.query(
       `UPDATE users
                             SET last_login_at = current_timestamp
                             WHERE username = $1
                             RETURNING username, last_login_at`,
-      [username]);
-
+      [username]
+    );
+    const u = result.rows[0];
+    if (u === undefined) throw new NotFoundError(`User cannot be found: ${username}`);
     //doesn't seem like we need to return anything here
   }
-
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
-
     const result = await db.query(
-      `SELECT 
-                                    username, 
-                                    first_name, 
+      `SELECT
+                                    username,
+                                    first_name,
                                     last_name
-                            FROM users`,
+                            FROM users`
     );
     const users = result.rows;
     return users;
-
   }
 
   /** Get: get user by username
@@ -103,19 +100,20 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
-
     const result = await db.query(
-      `SELECT 
-                                    username, 
-                                    first_name, 
+      `SELECT
+                                    username,
+                                    first_name,
                                     last_name,
                                     phone,
                                     join_at,
                                     last_login_at
                             FROM users
                             WHERE username = $1`,
-      [username]);
+      [username]
+    );
     const user = result.rows[0];
+    if (user === undefined) throw new NotFoundError(`User cannot be found: ${username}`);
     return user;
   }
 
@@ -126,42 +124,43 @@ class User {
    * where to_user is
    *   {username, first_name, last_name, phone}
    */
-
   static async messagesFrom(username) {
-
-    const messageResult = await db.query(
+    const result = await db.query(
       `SELECT m.id,
               m.to_username,
+              m.from_username,
               m.body,
               m.sent_at,
-              m.read_at
+              m.read_at,
+              u.username,
+              u.first_name,
+              u.last_name,
+              u.phone
          FROM messages AS m
-                JOIN users ON m.from_username = users.username
+                RIGHT OUTER JOIN users AS u ON m.to_username = users.username
          WHERE m.from_username = $1`,
-      [username]);
-    const messages = messageResult.rows;
+      [username]
+    );
+    const messagesResult = result.rows;
 
-    //may need to loop through the messages array that we have
-    // and set each object's to_user property
-    // to be the result of a query to the users table
-    //where we pull in their username, first/last name, phone
+    const u = result.rows[0];
+    if (u === undefined) throw new NotFoundError(`User cannot be found: ${username}`);
 
-    let messagesWithUserInfo = messages.map(async m => )
-
-
-
-//     const userResult = await db.query(
-//       `SELECT 
-//       username, 
-//       first_name, 
-//       last_name,
-//       phone
-// FROM users
-// WHERE username = $1`,
-// [username]);
-// const user = userResult.rows[0];
-
-
+    let messagesWithRecipientInfo = messagesResult.map((m) => {
+      return {
+        id: m.id,
+        to_user: {
+          username: m.username,
+          first_name: m.first_name,
+          last_name: m.last_name,
+          phone: m.phone,
+        },
+        body: m.body,
+        sent_at: m.sent_at,
+        read_at: m.read_at,
+      };
+    });
+    return messagesWithRecipientInfo;
   }
 
   /** Return messages to this user.
@@ -171,10 +170,40 @@ class User {
    * where from_user is
    *   {id, first_name, last_name, phone}
    */
-
   static async messagesTo(username) {
+    const result = await db.query(
+      `SELECT m.id,
+              m.from_username,
+              m.body,
+              m.sent_at,
+              m.read_at,
+              u.username,
+              u.first_name,
+              u.last_name,
+              u.phone
+         FROM messages AS m
+                JOIN users AS u ON m.from_username = users.username
+         WHERE m.to_username = $1`,
+      [username]
+    );
+    const messagesResult = result.rows;
+
+    let messagesWithRecipientInfo = messagesResult.map((m) => {
+      return {
+        id: m.id,
+        from_user: {
+          username: m.username,
+          first_name: m.first_name,
+          last_name: m.last_name,
+          phone: m.phone,
+        },
+        body: m.body,
+        sent_at: m.sent_at,
+        read_at: m.read_at,
+      };
+    });
+    return messagesWithRecipientInfo;
   }
 }
-
 
 module.exports = User;
